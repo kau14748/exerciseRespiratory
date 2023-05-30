1b13b422e4714c263c40c03c56c7574e7d7af9dc
kau14748 <133786598+kau14748@users.noreply.github.com>
2023-05-30 09:56:37 -0400

    Update CardTrick.java
    i have updated my personal information.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -45,18 +45,18 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Gurleen Kaur Gill, but you can call me buddy, friend ");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Be more active on indeed");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- studying");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
+        System.out.println("-- Risk taking");
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
